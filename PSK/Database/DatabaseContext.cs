e1b5c19b483914a1a1abb94258541912cde91c18
Vismantas Stonkus <vismantas.stonkus@bentley.com>
2021-02-13 16:12:15 +0200

    Initial

---
+++ PSK/Database/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Database
+{
+    public class DatabaseContext : DbContext
+        {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base (options) { }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        }
+    }
