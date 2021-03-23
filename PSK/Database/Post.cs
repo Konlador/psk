dd66b7c76dd1778444f83edcf81bca25f617cae5
DESKTOP-TTDRROK\Ben <ben>
2021-03-23 21:40:07 +0200

    pakeiciau krana

--- PSK/Database/Post.cs
+++ PSK/Database/Post.cs
@@ -5,7 +5,7 @@
         public int PostId { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
-
+        //ghjgfjh
         public int BlogId { get; set; }
         public Blog Blog { get; set; }
         }
