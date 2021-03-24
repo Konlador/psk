a6742f948d2afebe1e58cb657b18ba6a9a6a67ed
Dautartas <dautartasvalvonis@gmail.com>
2021-03-24 19:51:59 +0200

    merge this

---
+++ PSK/Frontend/psk-frontend-project/src/features/Upload/UploadFile.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
+export const UploadFile = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Fab color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
+      {/* <input type="file" accept="video/*"></input> */}
+      <form action="/upload">
+        <input type="file" accept="video/*"></input>
+        <input type="submit" value="Upload video"></input>
+      </form>
+    </div>
+  );
+};
