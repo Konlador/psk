85806a55644ed0bc6ca61374a901ff0a08e0f61b
Dautartas <dautartasvalvonis@gmail.com>
2021-03-24 19:51:59 +0200

    merge this

---
+++ PSK/Frontend/psk-frontend-project/src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Layout } from "../../components/Layout/Layout";
+
+import "./homePage.scss";
+
+function HomePage() {
+  return null;
+  //<Layout></Layout>;
+}
+
+export default HomePage;
