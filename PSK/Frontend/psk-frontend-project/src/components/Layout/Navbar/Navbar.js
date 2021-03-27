1c7d528c017ec942a6c562224050cd8570b25460
Dautartas <dautartasvalvonis@gmail.com>
2021-03-27 16:58:09 +0200

    Navbar changes

--- PSK/Frontend/psk-frontend-project/src/components/Layout/Navbar/Navbar.js
+++ PSK/Frontend/psk-frontend-project/src/components/Layout/Navbar/Navbar.js
@@ -1,8 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
-
 import { Toolbar, AppBar } from "@material-ui/core";
-
+import HomeRoundedIcon from "@material-ui/icons/HomeRounded";
+import BackupRoundedIcon from "@material-ui/icons/BackupRounded";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
 import "./navbar.scss";
 
 export const Navbar = (props) => {
@@ -11,14 +12,24 @@ export const Navbar = (props) => {
       <AppBar position="static" className="navbar__appbar">
         <Toolbar className="navbar__toolbar">
           <a href="/" className="navbar__link">
-            Home
+            <figure>
+              <HomeRoundedIcon />
+              <figcaption>Home</figcaption>
+            </figure>
           </a>
+
           <a href="/upload" className="navbar__link">
-            Upload
+            <figure>
+              <BackupRoundedIcon />
+              <figcaption>Upload</figcaption>
+            </figure>
           </a>
 
           <a href="/videos" className="navbar__link">
-            Your videos
+            <figure>
+              <VideoLibraryIcon />
+              <figcaption>Your videos</figcaption>
+            </figure>
           </a>
         </Toolbar>
       </AppBar>
