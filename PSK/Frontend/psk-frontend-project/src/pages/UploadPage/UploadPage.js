95ff20c78abb1bc50cbbefa73ead855f66716053
Dautartas <dautartasvalvonis@gmail.com>
2021-03-27 14:42:08 +0200

    Layout modified

--- PSK/Frontend/psk-frontend-project/src/pages/UploadPage/UploadPage.js
+++ PSK/Frontend/psk-frontend-project/src/pages/UploadPage/UploadPage.js
@@ -1,8 +1,4 @@
 import React from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { Navbar } from "../../components/Layout/Navbar/Navbar";
-// import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
 import { Layout } from "../../components/Layout/Layout";
 import { UploadFile } from "../../features/Upload/UploadFile";
 import "./uploadPage.scss";
@@ -10,14 +6,9 @@ import "./uploadPage.scss";
 function UploadPage() {
   return (
     <>
-      <React.Fragment>
-        <CssBaseline />
-        <Container maxWidth="lg">
-          <Layout name="UPLOAD FILES">
-            <UploadFile></UploadFile>
-          </Layout>
-        </Container>
-      </React.Fragment>
+      <Layout name="UPLOAD FILES">
+        <UploadFile></UploadFile>
+      </Layout>
     </>
   );
 }
