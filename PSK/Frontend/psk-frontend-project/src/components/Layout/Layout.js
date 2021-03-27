1c65c80e948944452e8c58829caca0f17146a6ff
Dautartas <dautartasvalvonis@gmail.com>
2021-03-27 14:42:08 +0200

    Layout modified

--- PSK/Frontend/psk-frontend-project/src/components/Layout/Layout.js
+++ PSK/Frontend/psk-frontend-project/src/components/Layout/Layout.js
@@ -1,32 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import { Navbar } from "./Navbar/Navbar";
-// import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
-import { BottomNavigation } from "@material-ui/core";
-import Breadcrumbs from "./Breadcrumbs/Breadcrumbs";
-
-// import { Header } from "./Header/Header";
-// import { Footer } from "./Footer/Footer";
+import Footer from "./Footer/Footer";
 import "./layout.scss";
-
-// import { Navbar } from "./Navbar/Navbar";
 
 export const Layout = (props) => {
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="lg">
-        <Navbar name={props.name}></Navbar>
-        <main-area>
-          {" "}
-          <Breadcrumbs></Breadcrumbs>
-          {props.children}
-        </main-area>
-        <BottomNavigation></BottomNavigation>
-      </Container>
-    </React.Fragment>
+    <div className="layout">
+      <Navbar name={props.name} className="layout__nav" />
+      <main className="layout__main"> {props.children}</main>
+      <Footer className="layout__footer"></Footer>
+    </div>
   );
 };
 Layout.propTypes = { children: PropTypes.node, name: PropTypes.string };
