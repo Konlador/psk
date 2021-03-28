e41c8b062f21a0228596dce6ff7150f7e28a7768
Vaidas <vaidasmac04@gmail.com>
2021-03-28 12:10:10 +0300

    Videos table added

--- PSK/Frontend/psk-frontend-project/src/features/DisplayVideos/DisplayVideos.js
+++ PSK/Frontend/psk-frontend-project/src/features/DisplayVideos/DisplayVideos.js
@@ -1,7 +1,107 @@
+import { React, useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import { MockVideoApi } from './MockVideoApi';
+
+const columns = [
+  {
+    id: 'title',
+    label: 'Title',
+    minWidth: 170,
+    align: 'left',
+  },
+  {
+    id: 'size',
+    label: 'Size',
+    minWidth: 170,
+    align: 'left',
+  },
+  {
+    id: 'uploadDate',
+    label: 'Upload date',
+    minWidth: 170,
+    align: 'left'
+  }
+];
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  container: {
+    maxHeight: '70vh',
+  },
+});
+
+const ROWS_PER_PAGE = 30;
+const videoApi = new MockVideoApi();
+
 export const DisplayVideos = () => {
-    return (
-      <div>
-       Videos List
-      </div>
-    );
+  const classes = useStyles();
+  const [page, setPage] = useState(0);
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+   setRows(videoApi.getVideos(0, ROWS_PER_PAGE));
+  }, []);
+
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+    setRows(videoApi.getVideos(newPage, ROWS_PER_PAGE));
   };
+
+  return (
+    <Paper className={classes.root}>
+      <TableContainer className={classes.container}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+            {rows.map((row) => {
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.title}>
+                  {columns.map((column) => {
+                    const value = row[column.id];
+                    return (
+                      <TableCell key={column.id} align={column.align}>
+                        {column.format && typeof value === 'number' ? column.format(value) : value}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[]}
+        component="div"
+        count={-1}
+        rowsPerPage={ROWS_PER_PAGE}
+        page={page}
+        onChangePage={handleChangePage}
+        labelDisplayedRows={({ from, to, count }) => `Displaying videos ${from}-${to}`}
+      />
+    </Paper>
+  );
+};
---
+++ PSK/Frontend/psk-frontend-project/src/features/DisplayVideos/MockVideoApi.js
@@ -0,0 +1,45 @@
+
+export class MockVideoApi {
+    getVideos(page, rowsPerPage){
+       let videos = [];
+
+       const startIndex = page*rowsPerPage;
+       const date = new Date();
+       for(let i = startIndex; i < startIndex+rowsPerPage; i++){
+            videos.push({
+                "title": "title"+(i+1), 
+                "size": Math.floor(Math.random() * 500)+1,
+                "uploadDate": this._getFormattedDate(this._subtractDays(date, Math.random() * 50+1))
+            });
+       }
+
+       return videos;
+    }  
+
+    _getFormattedDate(date){
+        let formattedDate = date.getFullYear();
+
+        formattedDate += "-";
+
+        if(date.getMonth() < 10){
+            formattedDate += "0";
+        }
+
+        formattedDate += date.getMonth();
+        formattedDate += "-";
+
+        if(date.getDate() < 10){
+            formattedDate += "0";
+        }
+
+        formattedDate += date.getDate();
+
+        return formattedDate;
+    }
+
+    _subtractDays(date, days){
+        const copy = new Date(Number(date))
+        copy.setDate(date.getDate() - days);
+        return copy;
+    }
+}
