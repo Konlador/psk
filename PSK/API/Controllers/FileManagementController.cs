236a181433378d3761d8a31a0b9faa519d29ee98
Vismantas Stonkus <vismantas.stonkus@bentley.com>
2021-04-10 21:55:27 +0300

    File rename

---
+++ PSK/API/Controllers/FileManagementController.cs
@@ -0,0 +1,46 @@
+﻿using Domain;
+using Domain.Management;
+using Domain.StorageItems;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace API.Controllers
+    {
+    [ApiController]
+    [Route("api/drive/{driveId:guid}")]
+    public class FileManagementController : ControllerBase
+        {
+        private readonly IManagementService m_managementService;
+
+        public FileManagementController(IManagementService managementService)
+            {
+            m_managementService = managementService;
+            }
+
+        [HttpGet]
+        [Route("files/{itemId:guid}/rename")]
+        public async Task<ActionResult<string>> Rename(
+            [FromRoute, ModelBinder] IDriveScopeFactory driveScopeFactory,
+            Guid itemId, [FromQuery, BindRequired] string newName, CancellationToken cancellationToken)
+            {
+            if(string.IsNullOrWhiteSpace(newName))
+                return BadRequest("");
+
+            newName = newName.Trim();
+
+            using var driveScope = driveScopeFactory.CreateInstance();
+            var item = await driveScope.StorageItems.GetAsync(itemId, cancellationToken);
+            if(item == null)
+                return NotFound();
+
+            item.Name = newName;
+
+            await driveScope.StorageItems.UpdateAsync(item, cancellationToken);
+            return Ok(item);
+            }
+        }
+    }
--- PSK/Domain.Impl/Management/ManagementService.cs
+++ PSK/Domain.Impl/Management/ManagementService.cs
@@ -22,8 +22,8 @@ namespace Domain.Impl.Management
             {
             var sasBuilder = new BlobSasBuilder
                                  {
-                                 BlobContainerName = "drives",
-                                 BlobName = $"{item.DriveId}/{item.Name}",
+                                 BlobContainerName = m_blobContainerClient.Name,
+                                 BlobName = $"{item.DriveId}/{item.Id}",
                                  Resource = "b",
                                  ExpiresOn = DateTimeOffset.UtcNow.AddHours(1),
                                  ContentDisposition = $"attachment;filename={item.Name}"
@@ -31,13 +31,13 @@ namespace Domain.Impl.Management
 
             sasBuilder.SetPermissions(BlobSasPermissions.Read);
 
-            return m_blobContainerClient.GetBlobClient($"{item.DriveId}/{item.Name}")
+            return m_blobContainerClient.GetBlobClient($"{item.DriveId}/{item.Id}")
                                         .GenerateSasUri(sasBuilder);
             }
 
         public async Task DeleteStorageItem(IDriveScope driveScope, StorageItem item)
             {
-            var blob = m_blobContainerClient.GetBlobClient($"{item.DriveId}/{item.Name}");
+            var blob = m_blobContainerClient.GetBlobClient($"{item.DriveId}/{item.Id}");
             await blob.GetBlobLeaseClient().BreakAsync();
             await blob.DeleteIfExistsAsync();
 
