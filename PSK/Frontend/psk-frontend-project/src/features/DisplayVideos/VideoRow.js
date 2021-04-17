b4fb255d319ea7269607c5e53bc0ac5f9a26286f
Vaidas <vaidasmac04@gmail.com>
2021-04-17 14:07:56 +0300

    Deleted icon added

--- PSK/Frontend/psk-frontend-project/src/features/DisplayVideos/VideoRow.js
+++ PSK/Frontend/psk-frontend-project/src/features/DisplayVideos/VideoRow.js
@@ -8,6 +8,7 @@ import PlayMenuItem from './PlayMenuItem';
 import RenameMenuItem from './RenameMenuItem';
 import CheckIcon from '@material-ui/icons/Check';
 import AutorenewIcon from '@material-ui/icons/Autorenew';
+import DeleteIcon from '@material-ui/icons/Delete';
 import { COLUMNS } from './VideoConstants';
 
 const initialState = {
@@ -49,7 +50,8 @@ export const VideoRow = ({video}) => {
     return (
         <TableRow onContextMenu = {handleRightClick} hover role="checkbox" 
         tabIndex={-1} key={currentVideo.id}>
-          <TableCell>{currentVideo.state === 0 ? <AutorenewIcon /> : <CheckIcon />}</TableCell>
+          <TableCell>{currentVideo.state === 0 ? <AutorenewIcon /> : 
+                currentVideo.state === 1 ? <CheckIcon /> :  <DeleteIcon />}</TableCell>
           {COLUMNS.map((column) => {
             const value = currentVideo[column.id];
             return (
