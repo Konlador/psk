5acd77551979a555422d0c778ba712a35fc0e701
Vaidas <vaidasmac04@gmail.com>
2021-05-18 00:58:29 +0300

    Search bar & sorting

---
+++ PSK/MediaDriveApp/ClientApp/src/features/DisplayVideos/SearchBar.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  search: {
+    position: 'relative',
+    border: '1px solid #D3D3D3',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginBottom: '10px',
+    width: '30%',
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    width: '100%',
+  },
+}));
+
+export default function SearchBar({ onChange }) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.grow}>
+        <div className={classes.search}>
+        <div className={classes.searchIcon}>
+            <SearchIcon />
+        </div>
+        <InputBase
+            placeholder="Search…"
+            classes={{
+            root: classes.inputRoot,
+            input: classes.inputInput,
+            }}
+            inputProps={{ 'aria-label': 'search' }}
+            onChange={(e) => onChange(e.target.value)}
+        />
+        </div>
+    </div>
+  );
+}
